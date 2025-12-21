0426a4bf7362520175445ef48c0d614749a9e9cb
Raqhib <raqhib.ataillah@gmail.com>
2025-12-21 23:18:20 +0800

    set up theme,font,color

--- app/src/main/java/id/co/mondo/ukhuwah/ui/theme/Color.kt
+++ app/src/main/java/id/co/mondo/ukhuwah/ui/theme/Color.kt
@@ -2,9 +2,10 @@ package id.co.mondo.ukhuwah.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Pink1 = Color(0xFFF7718D)
+val Pink2 = Color(0xFFFF928A)
+val Pink3 = Color(0xFFFCB0C0)
+
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
--- app/src/main/java/id/co/mondo/ukhuwah/ui/theme/Type.kt
+++ app/src/main/java/id/co/mondo/ukhuwah/ui/theme/Type.kt
@@ -2,33 +2,36 @@ package id.co.mondo.ukhuwah.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import id.co.mondo.ukhuwah.R
 
-// Set of Material typography styles to start with
+
+val poppins = FontFamily(
+    Font(R.font.poppinsmedium, FontWeight.Medium),
+    Font(R.font.poppinsbold, FontWeight.Bold),
+    Font(R.font.poppinssemibold, FontWeight.SemiBold),
+    )
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = poppins,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 14.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    titleMedium = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 14.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = poppins,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontSize = 14.sp,
+        lineHeight = 14.sp
+    ),
 )
