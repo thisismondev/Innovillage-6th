2520eb949c3d700b9f890864d2689fed3c957082
Raqhib <raqhib.ataillah@gmail.com>
2025-12-23 01:17:29 +0800

    add custom button and text field components

---
+++ app/src/main/java/id/co/mondo/ukhuwah/ui/components/ButtonCustom.kt
@@ -0,0 +1,53 @@
+package id.co.mondo.ukhuwah.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.co.mondo.ukhuwah.ui.theme.Innovillage6thTheme
+
+@Composable
+fun ButtonCustom(
+    label: String, modifier: Modifier, onCLick: () -> Unit
+) {
+
+    Button(
+        onClick = {
+            onCLick
+        },
+        modifier = modifier,
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    ) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.titleMedium,
+            color = Color.White,
+            modifier = Modifier.padding(6.dp),
+        )
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewButtonCompose() {
+    Innovillage6thTheme {
+        Column(Modifier.fillMaxSize()) {
+            ButtonCustom(
+                label = "Selanjutnya", modifier = Modifier, onCLick = {})
+        }
+    }
+}
---
+++ app/src/main/java/id/co/mondo/ukhuwah/ui/components/TextFieldCustom.kt
@@ -0,0 +1,122 @@
+package id.co.mondo.ukhuwah.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.co.mondo.ukhuwah.R
+import id.co.mondo.ukhuwah.ui.theme.Innovillage6thTheme
+
+@Composable
+fun TextFieldCustom(
+    modifier: Modifier,
+    values: String,
+    label: String,
+    onValueChange: (String) -> Unit,
+    isPasswordField: Boolean = false,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    enabled: Boolean = true
+) {
+
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = values,
+        onValueChange = {
+            onValueChange(it)
+        },
+        label = {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.titleMedium,
+                color = Color.Gray
+            )
+        },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = Color.LightGray,
+            focusedLabelColor = MaterialTheme.colorScheme.primary,
+            cursorColor = MaterialTheme.colorScheme.primary
+        ),
+
+        modifier = modifier,
+        singleLine = true,
+        textStyle = MaterialTheme.typography.titleMedium,
+        trailingIcon = {
+            if (isPasswordField) {
+                IconButton(
+                    onClick = { isPasswordVisible = !isPasswordVisible }
+                ) {
+                    Icon(
+                        painter = painterResource(
+                            if (isPasswordVisible)
+                                R.drawable.visibility
+                            else
+                                R.drawable.visibilityoff
+                        ),
+                        contentDescription = "Toggle Password Visibility",
+                        tint = Color.LightGray
+                    )
+                }
+            }
+        },
+        visualTransformation = if (isPasswordField && !isPasswordVisible) PasswordVisualTransformation() else VisualTransformation.None,
+        shape = RoundedCornerShape(14.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        enabled = enabled,
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun TextFieldCustomPreview() {
+    Innovillage6thTheme {
+        Column(
+            Modifier.fillMaxSize()
+        ) {
+
+            var email by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+
+
+
+            TextFieldCustom(
+                modifier = Modifier,
+                values = email,
+                label = "Email",
+                onValueChange = {email=it},
+                isPasswordField = false,
+                keyboardType = KeyboardType.Email,
+            )
+            TextFieldCustom(
+                modifier = Modifier,
+                values = password,
+                label = "Email",
+                onValueChange = {password=it},
+                isPasswordField = true,
+                keyboardType = KeyboardType.Password
+            )
+            
+        }
+    }
+}
